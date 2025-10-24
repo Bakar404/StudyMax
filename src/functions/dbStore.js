2ea948f8bc4f4695de90de839c25e76f0b4c3939
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-10-24 14:28:23 -0400

    further refactoring, generalized storing function

--- src/functions/dbStore.js
+++ src/functions/dbStore.js
@@ -10,22 +10,6 @@ export function createClassStore(db, storeName = "Classes") {
         console.log(`Object store '${storeName}' already exists.`);
     }
 }
-export default function storeClass(db, storeName = "Classes", data) {
-    const transaction = db.transaction(storeName, 'readwrite');
-    transaction.oncomplete = () => {
-        console.log('Transaction completed: database modification finished.');
-    }
-    transaction.onerror = () => {
-        console.error('Transaction not opened due to error: ', transaction.error);
-    }
-
-    const store = transaction.objectStore(storeName);
-    const request = store.add(data);
-    request.onsuccess = () => {
-        console.log('Data added to the store: ', data);
-    }
-}
-
 export function createTaskStore(db, storeName = "Tasks") {
     if (!db.objectStoreNames.contains(storeName)) {
         const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
@@ -37,7 +21,8 @@ export function createTaskStore(db, storeName = "Tasks") {
         console.log(`Object store '${storeName}' already exists.`);
     }
 }
-export function storeTask(db, storeName = "Tasks", data) {
+
+export function storeData(db, storeName, data) {
     const transaction = db.transaction(storeName, 'readwrite');
     transaction.oncomplete = () => {
         console.log('Transaction completed: database modification finished.');
