b0f44d8c9ce1f5ff41e60868639aed830fb63639
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-10-24 14:22:33 -0400

    refactored functions into create and store

--- src/functions/dbStore.js
+++ src/functions/dbStore.js
@@ -1,10 +1,16 @@
+export function createClassStore(db, storeName = "Classes") {
+    if (!db.objectStoreNames.contains(storeName)) {
+        const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+        objectStore.createIndex("courseTitle","courseTitle", { unique: true});
+        objectStore.createIndex("courseDescription","courseDescription", { unique: false, required:false});
+        objectStore.createIndex("days","days", { unique: false});
+        objectStore.createIndex("time","time", { unique: false});
+        console.log(`Object store '${storeName}' created.`);
+    } else {
+        console.log(`Object store '${storeName}' already exists.`);
+    }
+}
 export default function storeClass(db, storeName = "Classes", data) {
-    const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
-    objectStore.createIndex("courseTitle","courseTitle", { unique: true});
-    objectStore.createIndex("courseDescription","courseDescription", { unique: false, required:false});
-    objectStore.createIndex("days","days", { unique: false});
-    objectStore.createIndex("time","time", { unique: false});
-
     const transaction = db.transaction(storeName, 'readwrite');
     transaction.oncomplete = () => {
         console.log('Transaction completed: database modification finished.');
@@ -20,12 +26,18 @@ export default function storeClass(db, storeName = "Classes", data) {
     }
 }
 
+export function createTaskStore(db, storeName = "Tasks") {
+    if (!db.objectStoreNames.contains(storeName)) {
+        const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+        objectStore.createIndex("taskTitle","taskTitle", { unique: false});
+        objectStore.createIndex("taskDescription","taskDescription", { unique: false, required:false});
+        objectStore.createIndex("deadline","deadline", { unique: false});
+        console.log(`Object store '${storeName}' created.`);
+    } else {
+        console.log(`Object store '${storeName}' already exists.`);
+    }
+}
 export function storeTask(db, storeName = "Tasks", data) {
-    const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
-    objectStore.createIndex("taskTitle","taskTitle", { unique: false});
-    objectStore.createIndex("taskDescription","taskDescription", { unique: false, required:false});
-    objectStore.createIndex("dueDate","dueDate", { unique: false});
-
     const transaction = db.transaction(storeName, 'readwrite');
     transaction.oncomplete = () => {
         console.log('Transaction completed: database modification finished.');
