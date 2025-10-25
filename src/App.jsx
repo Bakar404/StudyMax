38225070e38aac46eb9035f746222e43e064ec5e
Denito37 <69406178+Denito37@users.noreply.github.com>
2025-10-25 09:00:38 -0400

    tested database connection

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import dbConnect from './functions/dbConnect.js';
 
 function App() {
+  dbConnect();
   const [count, setCount] = useState(0)
 
   return (
